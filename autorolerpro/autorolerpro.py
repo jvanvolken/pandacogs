86411bdd2498dfa0ae283d7b9a85fae289efb8c3
James Van Volkenburgh <jvanvolken1@gmail.com>
2023-12-19 11:25:04 -0600

    Fixed discord.client call

--- autorolerpro/autorolerpro.py
+++ autorolerpro/autorolerpro.py
@@ -23,7 +23,7 @@ class AutoRolerPro(commands.Cog):
         # Sends message in the command's origin channel
         await channel.send(f"This is where I'd list the games if I had any!!")
 
-    @discord.Client.event
+    @discord.client.event
     async def on_member_update(previous, current):
 
         # Get important information about the context of the command
