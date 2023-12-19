9019dc06473a3ca77328318d5a0ed286de8db015
James Van Volkenburgh <jvanvolken1@gmail.com>
2023-12-19 11:11:55 -0600

    Comment out test

--- autorolerpro/autorolerpro.py
+++ autorolerpro/autorolerpro.py
@@ -1,7 +1,3 @@
-# Image and File Manipulation Libraries
-from pathlib import Path
-from PIL import Image, ImageOps, ImageDraw, ImageFilter, ImageFont
-
 # Discord Bot Libraries
 import discord
 from redbot.core import commands
@@ -27,18 +23,18 @@ class AutoRolerPro(commands.Cog):
         # Sends message in the command's origin channel
         await channel.send(f"This is where I'd list the games if I had any!!")
 
-    @discord.Client.event
-    async def on_member_update(previous, current):
+    # @discord.Client.event
+    # async def on_member_update(previous, current):
 
-        # Get important information about the context of the command
-        channel = current.get_channel(665572348350693406)
-        member_name = current.display_name.encode().decode('ascii','ignore')
+    #     # Get important information about the context of the command
+    #     channel = current.get_channel(665572348350693406)
+    #     member_name = current.display_name.encode().decode('ascii','ignore')
 
-        # role = discord.utils.get(current.guild.roles, name="Gamer")
-        games = ["overwatch", "rocket league", "minecraft"]
+    #     # role = discord.utils.get(current.guild.roles, name="Gamer")
+    #     games = ["overwatch", "rocket league", "minecraft"]
 
-        # When somebody starts or stops playing a game
-        if current.activity and current.activity.name.lower() in games:
-            await channel.send(f"{member_name} started playing {current.activity.name}!")
-        elif previous.activity and previous.activity.name.lower() in games and not current.activity:
-            await channel.send(f"{member_name} stopped playing {current.activity.name}!")
+    #     # When somebody starts or stops playing a game
+    #     if current.activity and current.activity.name.lower() in games:
+    #         await channel.send(f"{member_name} started playing {current.activity.name}!")
+    #     elif previous.activity and previous.activity.name.lower() in games and not current.activity:
+    #         await channel.send(f"{member_name} stopped playing {current.activity.name}!")
