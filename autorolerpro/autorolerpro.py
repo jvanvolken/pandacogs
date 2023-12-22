3d856d71680e3f56f9eb68d426656d779eccfbce
James <jvanvolken1@gmail.com>
2023-12-22 15:54:55 -0600

    Fixed listtype to select for reply

--- autorolerpro/autorolerpro.py
+++ autorolerpro/autorolerpro.py
@@ -105,11 +105,11 @@ class AutoRolerPro(commands.Cog):
 
         if len(already_exists) > 0:
             if len(new_games) > 0:
-                await ctx.reply(f"Thanks for the contribution! I've added {', '.join(new_games)} to the list of games! I already have {', '.join(already_exists)}.", view = GameListView(ListType.Remove, [new_games, already_exists]))
+                await ctx.reply(f"Thanks for the contribution! I've added {', '.join(new_games)} to the list of games! I already have {', '.join(already_exists)}.", view = GameListView(ListType.Select, [new_games, already_exists]))
             else:
-                await ctx.reply(f"Thanks for the contribution! But I already have these!", view = GameListView(ListType.Remove, already_exists))
+                await ctx.reply(f"Thanks for the contribution! But I already have these!", view = GameListView(ListType.Select, already_exists))
         else:
-            await ctx.reply(f"Thanks for the contribution! Added {', '.join(new_games)} to the list of games!", view = GameListView(ListType.Remove, new_games))
+            await ctx.reply(f"Thanks for the contribution! Added {', '.join(new_games)} to the list of games!", view = GameListView(ListType.Select, new_games))
 
     @commands.command()
     async def remove_games(self, ctx):
