6cbf14dca35956ee154c3c4ba2b591566f43d6c5
James <jvanvolken1@gmail.com>
2023-12-22 17:19:58 -0600

    Updated fields

--- autorolerpro/autorolerpro.py
+++ autorolerpro/autorolerpro.py
@@ -131,7 +131,7 @@ class AutoRolerPro(commands.Cog):
     @commands.command()
     async def search_game(self, ctx, *, arg):
         """Searches IGDB for a matching game."""
-        db_json = post('https://api.igdb.com/v4/search', **{'headers' : db_header, 'data' : f'search "{arg}"; fields alternative_name,character,checksum,collection,company,description,game,name,platform,published_at,test_dummy,theme;'})
+        db_json = post('https://api.igdb.com/v4/search', **{'headers' : db_header, 'data' : f'search "{arg}"; fields *;'})
 
         
         reply = "Here are the results!\n"
