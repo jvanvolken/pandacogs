caeb970ac7a36dfb820270fb2c45d0ece9391f58
James <jvanvolken1@gmail.com>
2023-12-22 19:40:13 -0600

    Attempting to sort results

--- autorolerpro/autorolerpro.py
+++ autorolerpro/autorolerpro.py
@@ -135,7 +135,7 @@ class AutoRolerPro(commands.Cog):
         db_json = post('https://api.igdb.com/v4/games', **{'headers' : db_header, 'data' : f'search "{arg}"; fields name,summary,rating; limit 3; where summary != null; where rating != null;'}) #where description != null; where aggregated_rating != null;
         results = db_json.json()
 
-        #results = results.sort(key=itemgetter('rating'), reverse = True)
+        results = results.sort(key=itemgetter('rating'), reverse = True)
         print(str(results))
 
         if len(results) > 0:
