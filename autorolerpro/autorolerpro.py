fd6aea4b32e0067c48c2939fed1aebfc94644052
James Van Volkenburgh <jvanvolken1@gmail.com>
2024-01-10 16:04:51 -0600

    Updated try again message and original_message reference

--- autorolerpro/autorolerpro.py
+++ autorolerpro/autorolerpro.py
@@ -652,7 +652,7 @@ class AutoRolerPro(commands.Cog):
             elif len(already_exists) > 0:
                 game = list(already_exists.values())[0]
             elif len(failed_to_find) > 0:
-                await msg.reply(f"I could not find `{msg.content}` in the database! Please try again by replying to the original message!")
+                original_message = await msg.reply(f"I was unable to assign `{alias}` to a game - I couldn't find `{msg.content}` in the database! Please try again by replying to this message!")
             
         await msg.reply(f"Thanks, {msg.author.mention}! I've given {game['role']} an alias of `{alias}`.", files = await GetImages({game['name'] : game}))
 
