97331277913e2abdbcbc3e5a58f089d318f8919a
James Van Volkenburgh <jvanvolken1@gmail.com>
2024-03-10 07:25:58 -0500

    Added debug log for failed to find games

--- autorolerpro/autorolerpro.py
+++ autorolerpro/autorolerpro.py
@@ -443,6 +443,7 @@ async def AddGames(guild: discord.Guild, game_list: list):
         # Converts the json database response to a usable dictionary results variable
         results = db_json.json()
 
+        Log(str(results), LogType.Debug)
         # Exits if 'cause' exists in results, this is indicative of an error
         if len(results) == 0 or 'cause' in results[0]:
             Log(f"No Results Found for {game_name}: {str(results)}", LogType.Warning)
