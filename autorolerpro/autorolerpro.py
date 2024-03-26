9f52ef495e72dd2b1cde1c2027dc4bf1fdd314bc
James Van Volkenburgh <jvanvolken1@gmail.com>
2024-03-26 07:37:41 -0500

    Fixed GetRole to get actual role using ID

--- autorolerpro/autorolerpro.py
+++ autorolerpro/autorolerpro.py
@@ -444,8 +444,7 @@ def GetLowestScoringGame(black_list: list):
 # Finds role in guild - can create one if missing and remove the lowest score game's role if role count is maxed out
 async def GetRole(guild: discord.Guild, game_name: str, create_new: bool = False):
     # Search for an existing role
-    role: discord.Role = games[game_name]['role']
-    # role: discord.Role = discord.utils.get(guild.roles, name = role_name)
+    role: discord.Role = guild.get_role(games[game_name]['role'])
 
     # If no role is found and create_new is true, create a new role
     if not role and create_new:
