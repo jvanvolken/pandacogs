9f174df05be35d2e367b2b6d38b8f82e351ec562
James Van Volkenburgh <jvanvolken1@gmail.com>
2024-11-10 11:50:19 -0600

    Fixed missing directory

--- grokbot/filemanager.py
+++ grokbot/filemanager.py
@@ -11,6 +11,10 @@ class FileManager():
     config = None
     
     def Initialize(self, default_config: dict):
+        # Create directory path if it doesn't already exist
+        if not os.path.exists(docker_cog_path):
+            os.makedirs(docker_cog_path)
+
         # Initializes config 
         if os.path.isfile(config_file):
             with open(config_file, "r") as fp:
@@ -21,6 +25,7 @@ class FileManager():
                 if entry not in config:
                     config[entry] = value
         else:
+            # Set config to default config and write to file
             config = default_config
             with open(config_file, "w") as fp:
                 json.dump(config, fp, indent = 2, default = str, ensure_ascii = False)
