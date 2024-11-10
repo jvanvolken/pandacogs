3ebe6ac644c2948cafc9faac9e172913ab319de2
James Van Volkenburgh <jvanvolken1@gmail.com>
2024-11-09 21:32:01 -0600

    Added message option

--- grokbot/grokbot.py
+++ grokbot/grokbot.py
@@ -16,12 +16,12 @@ class GrokBot(commands.Cog):
         self.bot = bot
 
     @app_commands.command()
-    @app_commands.describe(personality="Describe who benjamin should be responding as.")
-    async def benjamin(self, interaction: discord.Interaction, personality: str = None):
+    @app_commands.describe(personality="Describe Benjamin's personality for this response!", message="Your message to Benjamin!")
+    async def benjamin(self, interaction: discord.Interaction, personality: str, message: str):
         """Replies to a message!"""
 
         # Get member that sent the command
         member = interaction.user
         guild  = interaction.guild
 
-        await interaction.response.send_message(f"Thank you for giving me the personality of {personality}")
+        await interaction.response.send_message(f"Thank you for giving me the personality of '{personality}' along with your message, '{message}'")
